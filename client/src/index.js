ceb908a2739cda9c49c1026c627856261f3686ba
Jellyyyyy05 <121446100+Jellyyyyy05@users.noreply.github.com>
2024-04-13 23:01:06 +0530

    Home page done

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer:rootReducer,
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-    <BrowserRouter>
+   <Provider store = {store}>
+
+  <BrowserRouter>
     <App />
-    </BrowserRouter>
+    </BrowserRouter>  
+   </Provider>
+    
   
 );
